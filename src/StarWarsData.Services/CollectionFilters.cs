ebf83159181dca5be3bac6bc76398fee99baa5fd
Patrick Magee <patrick.magee@live.co.uk>
2022-06-05 19:12:08 +0100

    - simpler regex mongodb filtering for dates with BBY/ABY - not all collections support timeline events - timeline collections put into appsettings.json

--- src/StarWarsData.Services/CollectionFilters.cs
+++ src/StarWarsData.Services/CollectionFilters.cs
@@ -1,50 +1,22 @@
+using System.Text.RegularExpressions;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using StarWarsData.Models;
 
 namespace StarWarsData.Services;
 
 public class CollectionFilters : Dictionary<string, FilterDefinition<BsonDocument>>
 {
-    private readonly FilterDefinition<BsonDocument> _dateCreated = Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date created");
-    private readonly FilterDefinition<BsonDocument> _date = Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date");
+    private readonly FilterDefinition<BsonDocument> _containsYearValueFilter = Builders<BsonDocument>.Filter.Regex("Data.Links.Content", new BsonRegularExpression(new Regex(".*\\s+(ABY|BBY)")));
+    private readonly FilterDefinition<BsonDocument> _containsYearLinkFilter = Builders<BsonDocument>.Filter.Regex("Data.Links.Href", new BsonRegularExpression(new Regex("_(ABY|BBY)")));
     
-    private readonly FilterDefinition<BsonDocument> _constructedOrDestroyed = Builders<BsonDocument>.Filter.Or(
-        Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Constructed"), 
-        Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Destroyed"));
-    
-    private readonly FilterDefinition<BsonDocument> _discoveredOrDestroyed = Builders<BsonDocument>.Filter.Or(
-        Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date discovered"), 
-        Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date destroyed"));
+    private FilterDefinition<BsonDocument> GenericDateRegexFilter => Builders<BsonDocument>.Filter.And(_containsYearValueFilter, _containsYearLinkFilter);
 
-    /// <summary>
-    /// Maybe this can come from appsettings.config and loaded at app startup
-    /// But really, this shouldnt be needed if we do Transformations into an Events collection...
-    /// </summary>
-    public CollectionFilters()
+    public CollectionFilters(Settings settings)
     {
-        Add("Event", _date);
-        Add("Election", _date);
-        Add("Law", _date);
-        Add("Battle", _date);
-        Add("Duel", _date);
-        Add("Mission", _date);
-        Add("Droid", _dateCreated);
-        Add("Artifact", _dateCreated);
-        Add("War", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Beginning"));
-        Add("Location", _constructedOrDestroyed);
-        Add("City", _constructedOrDestroyed);
-        Add("Campaign", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Begin"));
-        Add("Character", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Born"));
-        Add("Treaty", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date established"));
-        Add("Lightsaber", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date discovered"));
-        Add("Organization", Builders<BsonDocument>.Filter.Or(Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date founded"), Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Formed from")));
-        Add("Government", Builders<BsonDocument>.Filter.Or(Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date established"), Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Formed from")));
-        Add("Fleet", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Founding"));
-        Add("Disease",  Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date engineered"));
-        Add("Holocron_infobox", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date discovered"));
-        Add("Weapon", _discoveredOrDestroyed);
-        Add("Device", _dateCreated);
-        Add("Religion ", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date founded"));
-        Add("Trade_route ", Builders<BsonDocument>.Filter.AnyEq("Data.Label", "Date founded"));
+        foreach (var collection in settings.TimelineCollections.Distinct())
+        {
+            Add(collection, GenericDateRegexFilter);
+        }
     }
 }
